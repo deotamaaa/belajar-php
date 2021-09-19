d60d015c1ee89a41e463af7cd8739bb1dc454750
Deotama <deotama11@gmail.com>
2021-09-19 14:44:11 +0700

    third commit

---
+++ dasar PHP/Constant.php
@@ -0,0 +1,13 @@
+<?php
+// Constant bersifat immutable, tidak dapat dirubah isi nya setelah dideklarasi
+
+define("AUTHOR", "Deo tama");
+define("APP_VERSION", 100);
+
+echo "Author : ";
+echo AUTHOR;
+echo "\n";
+
+echo "App Version : ";
+echo APP_VERSION;
+echo "\n";
---
+++ dasar PHP/Variable.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Deo";
+$age = 22;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+echo "Age : ";
+echo $age;
